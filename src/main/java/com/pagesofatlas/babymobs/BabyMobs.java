9086c5f79aa1b33e7913ab383b6317fcb2a55962
Matt <matt@localhost>
2026-09-02 19:55:37 -0500

    Initial Baby Mobs 0.1.0 Fabric baseline

---
+++ src/main/java/com/pagesofatlas/babymobs/BabyMobs.java
@@ -0,0 +1,15 @@
+package com.pagesofatlas.babymobs;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class BabyMobs implements ModInitializer {
+    public static final String MOD_ID = "baby_mobs";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Baby Mobs initialized.");
+    }
+}
